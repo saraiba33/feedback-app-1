c9bde5297e29fa8e1c5e2ce745c4da30b4133bf1
Sara Ibarra <sara@Saras-MacBook-Pro.local>
2023-01-18 21:19:30 -0700

    add form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
 import Card from "./shared/Card"
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
     const [text, setText] = useState("");
-
+    const [Rating, setRating] = useState(10);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState("");
+    
     const handleTextChange = (e) => {
+        if(text === ""){
+            setMessage(null)
+            setBtnDisabled(true)
+        } else if(text !== "" & text.trim().length <= 10){
+            setMessage("Must be at least 10 characters")
+            setBtnDisabled(true)
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
 
@@ -12,7 +28,7 @@ function FeedbackForm() {
     <Card>
         <form>
             <h2>How would you rate your service with us?</h2>
-            {/*@todo - rating select component */}
+            <RatingSelect/>
         <div className="input-group">
             <input 
                 onChange={handleTextChange} 
@@ -20,8 +36,9 @@ function FeedbackForm() {
                 placeholder="Write a review" 
                 value={text}
             />
-            <button type="submit">Send</button>
+            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className="message">{message}</div>}
         </form>
     </Card>
   )
