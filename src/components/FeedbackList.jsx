5ffb052c696db798264ca8f671410cd5d33feadf
Sara Ibarra <sara@Saras-MacBook-Pro.local>
2023-04-12 15:54:51 -0600

    Add spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,48 +1,47 @@
-import {motion, AnimatePresence} from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
 
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const {feedback}= useContext(FeedbackContext)
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
-    return (
-      <div className="feedback-list">
-        <AnimatePresence>
+
+  return isLoading ? (
+    <Spinner/>
+  ) : (
+    <div className="feedback-list">
+      <AnimatePresence>
         {feedback.map((item) => (
-          <motion.div 
+          <motion.div
             key={item.id}
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            exit={{opacity: 0}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
           >
-          <FeedbackItem 
-            key={item.id} 
-            item={item} 
-          />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
-        </AnimatePresence>
-      </div>
-    );
-        
-        }
-  //version without animation
+      </AnimatePresence>
+    </div>
+  );
+}
+//version without animation
 
-  // return (
-  //   <div className="feedback-list">
-  //     {feedback.map((item) => (
-  //       <FeedbackItem 
-  //       key={item.id} 
-  //       item={item} 
-  //       handleDelete={handleDelete}
-  //       />
-  //     ))}
-  //   </div>
-  // );
-
+// return (
+//   <div className="feedback-list">
+//     {feedback.map((item) => (
+//       <FeedbackItem
+//       key={item.id}
+//       item={item}
+//       handleDelete={handleDelete}
+//       />
+//     ))}
+//   </div>
+// );
 
 export default FeedbackList;
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,29 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuid4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: "This is from context",
-            rating: 10
-        },{
-            id: 2,
-            text: "This is from context 2",
-            rating: 1
-        },{
-            id: 3,
-            text: "This is from context 3",
-            rating: 5
-        }
-    ]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     const deleteFeedback = (id) => {
         if (window.confirm("Are you sure you want to delete?")) {
@@ -53,6 +52,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
